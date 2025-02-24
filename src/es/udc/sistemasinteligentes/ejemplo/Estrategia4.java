c74462ac7044ba37bcb6d704d6858eedeae4cf03
alber180 <a.pazp@udc.es>
2025-02-24 14:30:53 +0100

    1 a creo que hecho

--- src/es/udc/sistemasinteligentes/ejemplo/Estrategia4.java
+++ src/es/udc/sistemasinteligentes/ejemplo/Estrategia4.java
@@ -1,7 +1,6 @@
 package es.udc.sistemasinteligentes.ejemplo;
 
 import es.udc.sistemasinteligentes.*;
-
 import java.util.ArrayList;
 
 public class Estrategia4 implements EstrategiaBusqueda {
@@ -10,10 +9,14 @@ public class Estrategia4 implements EstrategiaBusqueda {
     }
 
     @Override
-    public Estado soluciona(ProblemaBusqueda p) throws Exception{
-        ArrayList<Estado> explorados = new ArrayList<Estado>();
+    public Nodo[] soluciona(ProblemaBusqueda p) throws Exception {
+
+        ArrayList<Estado> explorados = new ArrayList<>();
         Estado estadoActual = p.getEstadoInicial();
         explorados.add(estadoActual);
+
+        Nodo nodo = new Nodo(estadoActual, null, null);
+        Nodo nodoHijo = null;
 
         int i = 1;
 
@@ -29,6 +32,12 @@ public class Estrategia4 implements EstrategiaBusqueda {
                 if (!explorados.contains(sc)) {
                     estadoActual = sc;
                     System.out.println((i++) + " - " + sc + " NO explorado");
+
+                    nodoHijo = new Nodo(estadoActual, nodo, acc);
+                    nodo = nodoHijo;
+
+                    // Mirar lo de estado actual para poner el padre
+
                     explorados.add(estadoActual);
                     modificado = true;
                     System.out.println((i++) + " - Estado actual cambiado a " + estadoActual);
@@ -40,6 +49,17 @@ public class Estrategia4 implements EstrategiaBusqueda {
             if (!modificado) throw new Exception("No se ha podido encontrar una solución");
         }
         System.out.println((i++) + " - FIN - " + estadoActual);
-        return estadoActual;
+        // Mandas todos con new 0
+        return reconstruyeSol(nodoHijo).toArray(new Nodo[0]);
+    }
+
+    public ArrayList<Nodo> reconstruyeSol(Nodo n){
+        ArrayList<Nodo> sol = new ArrayList<Nodo>();
+        Nodo nodo = n;
+        while (nodo!=null) {
+            sol.add(nodo);
+            nodo = nodo.getPadre();
+        }
+        return sol;
     }
 }
