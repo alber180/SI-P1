55e53d12ea9b213566e494178b5df62405a18e2d
alber180 <a.pazp@udc.es>
2025-02-24 14:31:20 +0100

    1 a creo que hecho

---
+++ src/es/udc/sistemasinteligentes/Nodo.java
@@ -0,0 +1,46 @@
+package es.udc.sistemasinteligentes;
+
+public class Nodo {
+
+    private Estado estado;
+    private Nodo padre;
+    private Accion accion;
+
+    public Nodo (Estado estado, Nodo padre, Accion accion) {
+        this.estado = estado;
+        this.padre = padre;
+        this.accion = accion;
+    }
+
+    public Estado getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Estado estado) {
+        this.estado = estado;
+    }
+
+    public Nodo getPadre() {
+        return padre;
+    }
+
+    public void setPadre(Nodo padre) {
+        this.padre = padre;
+    }
+
+    public Accion getAccion() {
+        return accion;
+    }
+
+    public void setAccion(Accion accion) {
+        this.accion = accion;
+    }
+
+    @Override
+    public String toString() {
+        return  "( " + estado +
+                ", " + padre  +
+                ", " + accion +
+                ')';
+    }
+}
